b1305350c4be02b3099b5f8024fc68fa98f1a379
heidisollie <heidisollie@users.noreply.github.com>
2019-06-02 22:24:16 +0200

    Initial Overleaf Import

---
+++ Code/tightened_constraints.m
@@ -0,0 +1,44 @@
+function [t_constraints, S_K, N] = tightened_constraints(constraints, system, disturbance)
+
+i=1;
+disturbance_set=Polyhedron(disturbance.E,disturbance.g);
+X(i) = disturbance_set;
+
+
+while not(system.A_K^i * disturbance_set <= system.alpha * disturbance_set)
+    i = i + 1;
+    X(i) = system.A_K * X(i-1) + disturbance_set;
+end
+N = i;
+%s = support(S,x), s = max_y x'y, y \in S
+for i = 1:size(constraints.C_K,1)
+    theta_N(i) = X(N).support(constraints.C_K(i,:)');
+end
+S_K = (1 - system.alpha)^(-1) * X(N);
+
+t_constraints.e = constraints.e - (1 - system.alpha)^(-1) * theta_N';
+t_constraints.C = constraints.C;
+t_constraints.D = constraints.D;
+
+constraints.d_K = t_constraints.e;
+%compute terminal constraints
+i=1;
+X_2(i) = Polyhedron(constraints.C_K, constraints.d_K);
+cond = true;
+while cond
+        i = i+1;
+        current_target.G = X_2(i-1).A;
+        current_target.h = X_2(i-1).b;
+        X_2(i) = br_set(system,constraints,current_target);
+        cond = and(not(X_2(i-1) == X_2(i)), i<=system.Nsim);
+end
+
+
+
+% initial condition
+t_constraints.E = S_K.A;
+t_constraints.f = S_K.b;
+
+%target
+t_constraints.G = X_2(i).A;
+t_constraints.h = X_2(i).b;
